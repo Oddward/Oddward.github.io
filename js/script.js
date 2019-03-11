2606c40ae4d21ce3569735d08bde617760581951
Mugtaba G <lilmujster@hotmail.com>
2019-03-12 00:08:46 +0800

    WIP2 - 1page
    main layout and about half the content

---
+++ js/script.js
@@ -0,0 +1,54 @@
+$(window).on('load', function() {
+    
+    //slider script
+    //counter
+    let slidenum = 0;
+    $('.slider .slide div').each(function(){
+        slidenum += 1;
+    });
+    
+    $('.slider .arrow').click(function(){
+        let n = activeImg();
+
+        $('.slider').removeClass('right left');
+
+        if($(this).hasClass('left')){
+            n -= 1;
+            $('.slider').addClass('left');
+            setTimeout(function(){
+                $('.slider .slide div.active').addClass('to_left');
+            }, 50)
+        }
+        else if($(this).hasClass('right')){
+            n +=1;
+            setTimeout(function(){
+                $('.slider .slide div.active').addClass('to_left');
+            }, 50)
+        }
+
+        if(n > slidenum) n = 1;
+        if(n < 1) n = slidenum;
+
+        setTimeout(function(){
+            $('.slider .slide div').removeClass('active');
+            $('.slider .slide div:nth-child('+n+')').addClass('active');
+
+            setTimeout(function(){
+                $('.slider .slide div').removeClass('to_left');
+                $('.slider .slide div').removeClass('to_right');
+            }, 500)
+        }, 50)
+    });
+
+    function activeImg(){
+        let n = 1;
+        $('.slider .slide div').each(function(index){
+            if($(this).hasClass('active')){
+                n += index;
+            }
+        });
+        return n;
+    }
+}
+
+)
