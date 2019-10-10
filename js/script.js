2517201f5bfab666fd0176a364f1dcebecc786dc
Mugtaba G <mugtaba.ga.su@gmail.com>
2019-10-11 01:02:26 +0200

    UI update of v2, new resume

--- js/script.js
+++ js/script.js
@@ -1,10 +1,21 @@
 $(window).on('load', function() {
     
     //Popper
-    $('#instalink').tooltip();
-    $('#linkedinlink').tooltip();
-    $('#codeplink').tooltip();
+    $('[data-toggle="tooltip"]').tooltip();
 
+    //detect scroll to bottom (migt make heavier)
+    /*function getDocHeight() {
+    	var d = document;
+    	return Math.max(
+    		d.body.scrollHeight, d.documentElement.scrollHeight,
+    		d.body.offsetHeight, d.documentElement.offsetHeight,
+    		d.body.clientHeight, d.documentElement.clientHeight
+    	);
+    }
+    $(window).scroll(function() {
+    	if($(window).scrollTop() + $(window).height() > (getDocHeight() - 100)){
+    		$('#floating-container').hide(3000);
+    	}
+    })*/
 }
-
 )
