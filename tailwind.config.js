d76d0a9b5b21aa6b8b354d7d27b39a74bec5afc3
Oddward G <mugitheg@yahoo.com>
2020-06-06 14:01:43 +0200

    starting on new layout

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  purge: [],
+  theme: {
+    fontFamily: {
+      display: ['Montserrat', 'sans-serif'],
+      body: ['Raleway', 'sans-serif'],
+    },
+    extend: {
+      colors: {
+        transparent: 'transparent',
+        black: '#000',
+        white: '#fff',
+        gray: {
+          200: '#EAEAEA',
+          800: '#353535'
+        },
+        blue: {
+          600: '#004B81'
+        }
+      }
+    },
+  },
+  variants: {},
+  plugins: [],
+}
